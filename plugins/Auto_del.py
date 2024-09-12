853399025a1179760676ff2d5e1ba0ad23f17442
animebeast07 <narutouzumakitamil2005@gmail.com>
2024-09-12 14:46:08 +0530

    Update Auto_del.py

--- plugins/Auto_del.py
+++ plugins/Auto_del.py
@@ -3,7 +3,7 @@ import asyncio
 def job():
     delete_old_files(DIRECTORY, FILE_LIFETIME_DAYS)
 
-schedule.every().day.at("01:00").do(job)
+asyncio.every().day.at("01:00").do(job)
 
 while True:
     schedule.run_pending()
